368eac157216cd92dc34be66e9f6a62afbaa6537
hongcha <hongcha@hongchadeiMac.local>
2024-04-07 19:36:42 +0800

    update: 登录页增加动画效果

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,37 +1,46 @@
 <template>
     <div
-        class="w-screen h-screen flex items-center justify-center select-none bg-gradient-to-r from-cyan-500 to-blue-500">
-        <div class="w-96 border px-6 py-8 shadow-md bg-white">
+        class="w-screen h-screen flex items-center justify-center select-none bg-gradient-to-r from-cyan-500 to-blue-500 relative">
+        <div class="w-96 border px-6 py-8 shadow-md bg-white z-10">
             <h2 class="text-center text-2xl">登录</h2>
             <div class="mt-4">
                 <el-form ref="loginFormRef" :model="loginForm" :rules="loginRules" size="large">
                     <el-form-item prop="username">
-                        <el-input v-model="loginForm.username" placeholder="请输入账号" />
+                        <el-input v-model="loginForm.username" placeholder="请输入账号"/>
                     </el-form-item>
                     <el-form-item prop="password">
-                        <el-input v-model="loginForm.password" type="password" show-password placeholder="请输入密码">
+                        <el-input v-model="loginForm.password" placeholder="请输入密码" show-password type="password">
                         </el-input>
                     </el-form-item>
                 </el-form>
-                <el-button class="w-full" size="large" type="primary" :loading="loading" :disabled="disabled"
-                    @click="onLogin(loginFormRef)">登录
+                <el-button :disabled="disabled" :loading="loading" class="w-full" size="large" type="primary"
+                           @click="onLogin(loginFormRef)">登录
                 </el-button>
             </div>
+        </div>
+        <div class="bubbles flex">
+            <template v-for="item in 50">
+                <span :style=css(item)></span>
+            </template>
         </div>
     </div>
 </template>
 
-<script setup lang="ts" name="login">
-import { useRouter } from "vue-router";
-import { ref, reactive } from 'vue';
-import { ElMessage } from "element-plus";
-import type { ElForm } from "element-plus";
-import { useUserStore } from '@/store/modules/user';
+<script lang="ts" name="login" setup>
+import {useRouter} from "vue-router";
+import {reactive, ref} from 'vue';
+import type {ElForm} from "element-plus";
+import {ElMessage} from "element-plus";
+import {useUserStore} from '@/store/modules/user';
 
 type FormInstance = InstanceType<typeof ElForm>;
 
 const userStore = useUserStore();
 const router = useRouter()
+
+const css = (item: number) => {
+    return {'--i': Math.floor(Math.random() * (item + 1)).toString()}
+}
 
 const loginFormRef = ref<FormInstance>()
 
@@ -41,8 +50,8 @@ const loginForm = reactive({
 })
 
 const loginRules = reactive({
-    username: [{ required: true, message: "请输入用户名", trigger: "blur" }],
-    password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+    username: [{required: true, message: "请输入用户名", trigger: "blur"}],
+    password: [{required: true, message: "请输入密码", trigger: "blur"}]
 });
 
 const loading = ref(false)
@@ -56,7 +65,10 @@ const onLogin = (formEl: FormInstance | undefined) => {
         loading.value = true
         disabled.value = true
         userStore.setToken('11111')
-        userStore.setUserInfo({ username: 'admin', avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif' })
+        userStore.setUserInfo({
+            username: 'admin',
+            avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif'
+        })
         router.replace('/home').then(() => {
             ElMessage.success('登录成功')
         }).finally(() => {
@@ -64,5 +76,39 @@ const onLogin = (formEl: FormInstance | undefined) => {
         })
     })
 }
-
 </script>
+
+<style lang="css" scoped>
+
+.bubbles {
+    position: absolute;
+
+    span {
+        width: 30px;
+        height: 30px;
+        background-color: #79bbff;
+        margin: 0 20px;
+        border-radius: 50%;
+        box-shadow: 0 0 0 10px #4fc3dc44,
+        0 0 50px #4fc3dc,
+        0 0 100px #4fc3dc;
+        animation: animate calc(120s / var(--i)) linear infinite;
+
+        &:nth-child(even) {
+            background-color: #ff2d75;
+            box-shadow: 0 0 0 10px #ff2d7544,
+            0 0 50px #ff2d75,
+            0 0 100px #ff2d75;
+        }
+    }
+}
+
+@keyframes animate {
+    0% {
+        transform: translateY(100vh) scale(0);
+    }
+    100% {
+        transform: translateY(-100vh) scale(1);
+    }
+}
+</style>
